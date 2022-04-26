1dde940b69c0eb288ca3fb5282d782ce71cdfb82
Elvissamir <elvissamir18@gmail.com>
2022-04-26 17:19:07 -0400

    Add reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,26 +1,18 @@
-import * as actions from './actionTypes'
+let lastId = 0
 
-let lastId = 0;
+function reducer(state = [], action) {
+    switch (action.type) {
+        case "bugAdded":
+            return [...state, 
+                { 
+                    id: lastId++,
+                    description: action.payload.description, 
+                    resolved: false
+                }]
+        case "bugRemoved":
+            return state.filter(bug => bugs.id !== action.payload.id)
+        default:
+            return state
+    }
 
-export default function reducer(state = [], action) {
-
-    
-    if (action.type == actions.BUG_ADDED)
-        return [
-            ...state,
-            {
-                id: ++lastId,
-                description: action.payload.description,
-                resolved: false
-            }
-        ]
-    else if (action.type == actions.BUG_REMOVED) 
-        return state.filter(bug => bug.id !== action.payload.id)
-    
-    else if (action.type == actions.BUG_RESOLVED) 
-        return state.map(bug => bug.id !== action.payload.id ? bug : {...bug, resolved:true})
-
-
-    return state;
-    
 }
